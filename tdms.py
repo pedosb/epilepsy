a6abe6c093d65190b9afd7df533260e55373c3fc
Pedro Batista <pedosb@gmail.com>
2012-02-23 08:21:37 -0300

    Select the time interval to plot using -t

--- tdms.py
+++ tdms.py
@@ -23,13 +23,15 @@ import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 
-class TdmsSyntaxError():
+class TdmsSyntaxError(Exception):
 	pass
 
 class Tdms():
-	def __init__(self, fn=None):
-		self.load(fn)
-	def load(self, fn):
+	def __init__(self, fn=None, ti=None):
+		self.load(fn, ti)
+	def load(self, fn, ti=None):
+		if ti is None:
+			ti = [0, -1]
 		f = open(fn)
 		if f.readline().strip() != 'channel names:':
 			raise TdmsSyntaxError("Can't find channels names")
@@ -42,21 +44,36 @@ class Tdms():
 		if f.readline().strip() != 'dt:':
 			raise TdmsSyntaxError("Can't find dt flag")
 		self.fs = 1/float(f.readline())
+		self.start = ti[0] * self.fs
+		self.stop = ti[1] * self.fs
 
 		if f.readline().strip() != 'data:':
 			raise TdmsSyntaxError("Can't find data flag")
 
 		self.data = []
 		self.wav = []
+		self.data_tell = f.tell()
+		c = -1
 		while True:
 			line = f.readline()
-			if line == '':
+			c += 1
+			if c < self.start:
+				continue
+			if line == '' or (c > self.stop and self.stop > 0):
 				break
 #			self.data.append([float(n) for n in line.split('\t')])
 			i, j = line.split('\t')
 			self.wav.append(float(i) - float(j))
 
 		self.size = len(self.wav)
+		if self.size < self.stop - self.start:
+			c = 0
+			f.seek(self.data_tell)
+			while True:
+				if f.readline() == '':
+					break
+				c += 1
+			raise TdmsSyntaxError("Can't select time interval, must be between [0, %.2f]" % (c / self.fs))
 #		if len(self.channels) == 2:
 #			self.wav = [i - j for i, j in self.data]
 #			del self.data
@@ -87,15 +104,17 @@ class Tdms():
 if __name__=='__main__':
 	parser = argparse.ArgumentParser(description='Manipulate TDMS file')
 	parser.add_argument('-f', '--input-file', required=True,
-		help='The TDMS (txt) file to be used')
+			help='The TDMS (txt) file to be used')
 	parser.add_argument('-p', '--plot-wav-form', action='store_true',
-		help='plot the wave form of a two channels TDMS file')
+			help='plot the wave form of a two channels TDMS file')
 	parser.add_argument('-s', '--plot-specgram', action='store_true',
-		help='plot the spetogram of a two channels TDMS file')
+			help='plot the spetogram of a two channels TDMS file')
 	parser.add_argument('-r', '--plot-fft', action='store_true',
-		help='plot the fft of a two channels TDMS file')
+			help='plot the fft of a two channels TDMS file')
+	parser.add_argument('-t', '--time-interval', nargs=2, type=int,
+			help='select the time interval to plot')
 	args = parser.parse_args()
-	t = Tdms(args.input_file)
+	t = Tdms(args.input_file, args.time_interval)
 	if args.plot_wav_form:
 		t.plot()
 	if args.plot_specgram:
